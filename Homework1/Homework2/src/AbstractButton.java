8baea443ad46cdaf293c036c8dba38c0d94cf93c
dddbdb <80088870+dddbdb@users.noreply.github.com>
2021-03-18 22:51:54 +0800

    Delete Homework1 directory

--- Homework1/Homework2/src/AbstractButton.java
+++
@@ -1,6 +0,0 @@
-abstract class AbstractButton{
-void view(){
-
-}
-    }
-
--- Homework1/Homework2/src/main.java
+++
@@ -1,6 +0,0 @@
-public class main  {
-    public static void main(String[] args) {
-        LoginForm lf=new LoginForm();
-        lf.display();
-    }
-}
